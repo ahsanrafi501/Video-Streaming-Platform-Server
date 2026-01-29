27aba3318bdbca9f361fc37178dc2fc47eb5b886
Ahsan Habib <habib23105341021@diu.edu.bd>
2026-01-29 15:54:50 +0600

    add utils

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import connectDB from "./db/index.js";
 
 
 dotenv.config({
-    path:'./env'
+    path: './env'
 })
 
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", error => {
+            console.log("Error: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`)
+        })
+    })
+    .catch(error => {
+        console.log("MongoDB connection Failed !!", error)
+    })
 
 
 /*
